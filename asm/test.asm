c5d650a5757245646b7d161c207bf6768e7d1b53
ChaofengWu0 <82070917+ChaofengWu0@users.noreply.github.com>
2022-06-01 01:38:10 +0800

    Update test.asm

--- asm/test.asm
+++ asm/test.asm
@@ -1,13 +1,15 @@
+#2号寄存器存要读取的地址
 lui $2,0x1001
 ori $2,$2,0x28
 #1号寄存器存储总共的位数
 addi $1,$7,0
+lw   $3,0($2)
 circle_test:
 addi $1,$1,-1
 #把值存在3号寄存器中
 lw   $3,0($2)
 #加4
-addi $2,$2,4
+addi $3,$3,4
 sw   $3,0xC60($28)
 #5号寄存器用来判断
 lw    $5,0xC40($28)
@@ -16,6 +18,4 @@ during_test:
 lw    $5,0xC40($28)
 bne  $5,$22,during_test
 bne $1,$0,circle_test
-
 end_test:
-j end_test
